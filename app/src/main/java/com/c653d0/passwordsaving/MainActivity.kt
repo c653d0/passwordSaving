9926fda740ce92d1975d264a7090671168aa1a15
c653d0 <1664303343@qq.com>
2022-02-24 16:10:56 +0800

    first init

---
+++ app/src/main/java/com/c653d0/passwordsaving/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.c653d0.passwordsaving
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
